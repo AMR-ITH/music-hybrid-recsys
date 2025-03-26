c048b6d45c174af52e752eb66c4635b586b8a0d2
Amrith <amrithputtur@gmail.com>
2025-03-26 13:16:13 +0530

    intial deployement testing

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -19,6 +19,7 @@ if [ "$(docker ps -aq -f name=hybrid_recsys)" ]; then
     docker rm hybrid_recsys
 fi
 
+
 echo "Starting new container..."
 docker run -d --memory=512m --memory-swap=1g -p 8000:8000 --name hybrid_recsys 116981799876.dkr.ecr.ap-south-1.amazonaws.com/spotify_recsys:latest
 
